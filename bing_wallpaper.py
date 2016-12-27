e26db8a2d024a4b3cf47930b72088a7393e03a52
Miguel Garrido <mgarrido@gmail.com>
2016-12-27 09:03:55 -0500

    Update bing_wallpaper.py
    Made the string quotes uniform across the entire script

--- bing_wallpaper.py
+++ bing_wallpaper.py
@@ -9,19 +9,20 @@ import requests
 import bs4
 
 # define destination root directory
-root_dir = "/Users/username/Pictures/Bing"
+root_dir = '/Users/username/Pictures/Bing'
 arch_dir = os.path.join(root_dir, "Archive")
+today = str(datetime.date.today())
 
 # create our archive directory
 if not os.path.exists(arch_dir):
     os.mkdir(arch_dir, stat.S_IRWXU|stat.S_IRGRP|stat.S_IXGRP|stat.S_IROTH|stat.S_IXOTH)
 
 # find today's image url
-feed_url = "https://www.iorise.com/?feed=rss2"
-feed = bs4.BeautifulSoup(requests.get(feed_url).content, "lxml")
+feed_url = 'https://www.iorise.com/?feed=rss2'
+feed = bs4.BeautifulSoup(requests.get(feed_url).content, 'lxml')
 for item in feed.channel:
-    if "Worldwide, %s" % str(datetime.date.today()) in str(item.title):
-        image_url = item.find("content:encoded").a.get("href")
+    if 'Worldwide, %s' % today in str(item.title):
+        image_url = item.find('content:encoded').a.get('href')
 
 # calculate md5 signature and confirm we don't already have this file downloaded
 url_sig = hashlib.md5(image_url).hexdigest()
@@ -34,7 +35,7 @@ for f in os.listdir(root_dir):
         os.rename(os.path.join(root_dir, f), os.path.join(root_dir, arch_dir, f))
 
 # download the file
-image_fn = os.path.join(root_dir, "%s_%s.jpg" % (str(datetime.date.today()), url_sig))
+image_fn = os.path.join(root_dir, '%s_%s.jpg' % (today, url_sig))
 r = requests.get(image_url, stream=True)
 if r.status_code == 200:
     with open(image_fn, 'wb') as f:
