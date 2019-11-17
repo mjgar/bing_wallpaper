409bd27d27039ee31cba6e1208f94c039fb26e40
Miguel Garrido <mgarrido@gmail.com>
2019-11-16 22:36:32 -0500

    Update to use new script (#1)

--- README.md
+++ README.md
@@ -1,10 +1,28 @@
-bing_wallpaper
+bing_wallpaper.py
 ==============
 
-Script to download latest Bing wallpaper
+Script to download current background from [https://bing.com](bing.com) to be used as a wallpaper.
+
+Running the script
+==========
+
+### Creating the environment
+Either with conda
+```
+conda create -n bing -c defaults -c conda-forge python=3.7 requests beautifulsoup4 funcy
+```
+
+Or with pip
+```
+pip install funcy requests beautifulsoup4
+```
+
+### Running
+We can now run the script from the conda environment or venv/python environment _eg._ `python bing_wallpaper.py --dest ~/Desktop`
 
 More details
 ========
-http://blippy.net/2016/12/bing-wallpaper-on-a-mac-2-0/
 
-http://blippy.net/2014/11/bing-wallpaper-on-a-mac/
+The next time macOS changes the wallpaper it should find the the new file and update the wallpaper, assuming it is configured to rotate the wallpaper using the script's destination directory as the source. I run this script every 5 minutes and have macOS configured to rotate the wallpaper every 15.
+
+Also on my blog, https://blippy.net/2019/11/16/bing-wallpaper-on-a-mac-3-0/.
--- bing_wallpaper.py
+++ bing_wallpaper.py
@@ -1,51 +1,94 @@
-#!/Users/username/anaconda/bin/python
-
-import os
-import stat
-import hashlib
-import sys
+import argparse
 import datetime
-import requests
+import hashlib
+import logging
+import os
+import re
+import shutil
+
 import bs4
+import funcy
+import requests
 
-# define destination root directory
-root_dir = '/Users/username/Pictures/Bing'
-arch_dir = os.path.join(root_dir, 'Archive')
-today = str(datetime.date.today())
 
-# create our archive directory
-if not os.path.exists(arch_dir):
-    os.mkdir(arch_dir, stat.S_IRWXU|stat.S_IRGRP|stat.S_IXGRP|stat.S_IROTH|stat.S_IXOTH)
+logging.basicConfig(level=logging.INFO,
+                    format='[%(asctime)s] - %(levelname)s - %(message)s')
+log = logging.getLogger()
 
-# find today's image url
-feed_url = 'https://www.iorise.com/?feed=rss2'
-feed = bs4.BeautifulSoup(requests.get(feed_url).content, 'lxml')
-for item in feed.channel:
-    if 'Worldwide, %s' % today in str(item.title):
-        image_url = item.find('content:encoded').a.get('href')
-        break
 
-# get files in root_dir
-root_dir_files = os.listdir(root_dir)
+@funcy.retry(3, timeout=lambda a: 2 ** a)
+def main(dest: str):
+    """
+    @param: dest Destination for downloaded image
+    Find the URL of today's wallpaper, download it if the URL's md5sum doesn't exist.
+    Destination filename will be YYYY-mm-dd_{md5dum}.jpg
+    """
+    bing_url = 'https://bing.com'
+    archive_dir = os.path.join(dest, 'Archive')
 
-# calculate md5 signature and confirm we don't already have this file downloaded
-url_sig = hashlib.md5(image_url).hexdigest()
-if any(url_sig in f for f in root_dir_files):
-    sys.exit(0)
+    try:
+        log.info(f"Connecting to {bing_url}")
+        r = requests.get(bing_url)
+        if not r.ok:
+            raise f"{r.reason}"
+    except:
+        log.error(f"Could not get data from {bing_url}. Exiting.")
+        return
 
-# archive existing .jpg files
-for f in root_dir_files:
-    if os.path.isfile(os.path.join(root_dir, f)) and os.path.splitext(f)[1] == '.jpg':
-        os.rename(os.path.join(root_dir, f), os.path.join(root_dir, arch_dir, f))
+    img_cont = bs4.BeautifulSoup(
+        r.content, 'html.parser').find_all('div', class_='img_cont')
+    if len(img_cont) < 0:
+        log.error(f"Could not parse html from {bing_url}. Exiting.")
+        return
+    url = bing_url + re.search(r'\((.+)\)', str(img_cont)).group(1)
+    log.info(f"Image url parsed from html as {url}")
+    md5sum = hashlib.md5(url.encode('utf-8')).hexdigest()
+    log.info(f"Hash of {url} calculated as {md5sum}")
 
-# download the file
-image_file = os.path.join(root_dir, '%s_%s.jpg' % (today, url_sig))
-r = requests.get(image_url, stream=True)
-if r.status_code == 200:
-    with open(image_file, 'wb') as f:
-        for chunk in r:
-            f.write(chunk)
+    # Stop if we have this checksum in dest
+    existing_files = os.listdir(dest)
+    log.debug(f"Existing files in {dest} are {existing_files}")
+    if any(md5sum in f for f in existing_files):
+        log.info(f"Hash {md5sum} found in {dest}. Exiting.")
+        return
 
-# The next time macOS changes the wallpaper it should find the the new file and update the wallpaper
-# assuming macOS is configured to rotate the wallpaper from /Users/username/Pictures/Bing at some interval
-# I run this script every 5 minutes and configure macOS to update the wallpaper every 15
+    # Build the filename
+    image_file = f"{datetime.date.today().isoformat()}_{md5sum}.jpg"
+    image_fullname = os.path.join(dest, image_file)
+
+    # Download the file
+    try:
+        log.info(f"Downloading {url} to {image_fullname}")
+        r = requests.get(url, allow_redirects=True)
+        if r.ok:
+            with open(image_fullname, 'wb') as f:
+                log.debug(f"Writing to disk as {image_fullname}")
+                f.write(r.content)
+        else:
+            log.error(f"Could not download {url}, reason: {r.reason}")
+    except:
+        log.error(f"Could not download {url} to {image_fullname}")
+        return
+
+    # Archive the existing file
+    if os.path.isdir(archive_dir):
+        for f in existing_files:
+            if f.endswith('.jpg'):
+                log.info(f"Archiving {f} to {archive_dir}")
+                shutil.move(os.path.join(dest, f), archive_dir)
+
+    # Done
+    log.info('Done')
+
+
+if __name__ == '__main__':
+    """ Initialize a (very basic) argument parser with destination directory
+        and download the image there archiving any existing .jpgs to {dest}/Archive
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dest', type=str, required=True)
+    args = parser.parse_args()
+    if not os.path.isdir(args.dest):
+        log.error(f"{args.dest} is not a directory. Exiting.")
+    else:
+        main(args.dest)
